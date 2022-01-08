1a2ae41f5af80e0935d95b30dc09244f3d02bb02
nancy hernandez <hernandezyane@hotmail.com>
2022-01-08 14:27:15 -0300

    construcción de login y register con estilos

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Desde Login</h1>;
+  const [usuario, setUsuario] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    //Validar que no haya campos vacíos
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+        <small className="form-link">
+          <p>¿No tienes cuenta?</p>
+          <Link to={"/register"} className="enlace-cuenta">
+            Regístrate
+          </Link>
+        </small>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <h1>Desde Register</h1>;
+  const [usuario, setUsuario] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    confirmar: "",
+  });
+
+  const { nombre, email, password, confirmar } = usuario;
+
+  const onChange = (e) => {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    //Validar que no haya campos vacios
+
+    //Password mínimo 6 caracteres un
+
+    //Los 2 password deben ser iguales
+
+    //Pasarlo al action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Obtener una cuenta</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              placeholder="Tu Nombre"
+              value={nombre}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Tu Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="confirmar">Confirmar Password</label>
+            <input
+              type="password"
+              id="confirmar"
+              name="confirmar"
+              placeholder="Repite tu Password"
+              value={confirmar}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Registrarme"
+            />
+          </div>
+        </form>
+        <small className="form-link">
+          <p>¿Ya estas registrado?</p>
+          <Link to={"/"} className="enlace-cuenta">
+            Login o inicio de sesión
+          </Link>
+        </small>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
